0eea1648bf1c6cb3cc37b05d6fc589d5c5b2ec31
root <root@DESKTOP-FDAPO4D>
2024-03-01 01:02:22 -0500

    added sidenav files

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 0 */
+function closeNav()
+{
+    document.getElementById("sideNavigationMenu").style.width = "0";
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav()
+{
+    document.getElementById("sideNavigationMenu").style.width = "250px";
+}
+
