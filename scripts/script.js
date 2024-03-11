473266caf4802d48e0d158be697fe53bb8d72a38
root <root@DESKTOP-FDAPO4D>
2024-03-11 08:58:29 -0500

    Added colors, fonts, and buttons. Reorganized structure of the web page

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
-/* Set the width of the side navigation to 0 */
+/* Sidenav: Set the width to 0 */
 function closeNav()
 {
     document.getElementById("sideNavigationMenu").style.width = "0";
 }
 
-/* Set the width of the side navigation to 250px */
+/* Sidenav: Set the width to 250px */
 function openNav()
 {
     document.getElementById("sideNavigationMenu").style.width = "250px";
 }
-
