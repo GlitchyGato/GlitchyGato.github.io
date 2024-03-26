f7902a7eb289350ac1188bcac19f65582d260060
root <root@DESKTOP-FDAPO4D>
2024-03-26 13:31:35 -0500

    Standardized page colors (hr and sidenav), added image carousel, and changed profile picture.

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,34 @@ function openNav()
 {
     document.getElementById("sideNavigationMenu").style.width = "250px";
 }
+
+/* Image carousel */
+let slideIndex = 1;
+showSlides(slideIndex + 1);
+
+/* Next/previous controls */
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("imgCarousel");
+    let dots = document.getElementsByClassName("dot");
+
+    /* In case we are in the last image */
+    if (n > slides.length) {slideIndex = 1}   
+    /* In case we are in the first image and go backwards */ 
+    if (n < 1) {slideIndex = slides.length}
+     
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";   
+} 
